24d493140e86ee0550aa2a8a06e3ce41197217ec
Masa <katsukatsu.masa.0522@gmail.com>
2021-06-29 22:35:55 +0900

    todo list first commit

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-
-// ★STEP2
-// https://jp.vuejs.org/v2/examples/todomvc.html
 var STORAGE_KEY = 'todos-vuejs-demo'
 var todoStorage = {
   fetch: function () {
@@ -17,93 +14,63 @@ var todoStorage = {
 }
 
 
-// ★STEP1
-new Vue({
-  el: '#app',
-
-  data: {
-    // ★STEP5 localStorage から 取得した ToDo のリスト
-    todos: [],
-    // ★STEP11 抽出しているToDoの状態
-    current: -1,
-    // ★STEP11＆STEP13 各状態のラベル
-    options: [
-      { value: -1, label: 'すべて' },
-      { value: 0, label: '作業中' },
-      { value: 1, label: '完了' }
-    ]
-  },
-
-  computed: {
-
-    // ★STEP12
-    computedTodos: function () {
-      return this.todos.filter(function (el) {
-        return this.current < 0 ? true : this.current === el.state
-      }, this)
+var app = new Vue ({
+    el: "#app",
+    data: {
+        title: "",
+        things: [],
     },
-
-    // ★STEP13 作業中・完了のラベルを表示する
-    labels() {
-      return this.options.reduce(function (a, b) {
-        return Object.assign(a, { [b.value]: b.label })
-      }, {})
-      // キーから見つけやすいように、次のように加工したデータを作成
-      // {0: '作業中', 1: '完了', -1: 'すべて'}
-    }
-  },
-
-  // ★STEP8
-  watch: {
-    // オプションを使う場合はオブジェクト形式にする
-    todos: {
-      // 引数はウォッチしているプロパティの変更後の値
-      handler: function (todos) {
-        todoStorage.save(todos)
-      },
-      // deep オプションでネストしているデータも監視できる
-      deep: true
-    }
-  },
-
-  // ★STEP9
-  created() {
-    // インスタンス作成時に自動的に fetch() する
-    this.todos = todoStorage.fetch()
-  },
-
-  methods: {
-
-    // ★STEP7 ToDo 追加の処理
-    doAdd: function(event, value) {
-      // ref で名前を付けておいた要素を参照
-      var comment = this.$refs.comment
-      // 入力がなければ何もしないで return
-      if (!comment.value.length) {
-        return
-      }
-      // { 新しいID, コメント, 作業状態 }
-      // というオブジェクトを現在の todos リストへ push
-      // 作業状態「state」はデフォルト「作業中=0」で作成
-      this.todos.push({
-        id: todoStorage.uid++,
-        comment: comment.value,
-        state: 0
-      })
-      // フォーム要素を空にする
-      comment.value = ''
+    methods: {
+        add: function(){
+            if(this.newthings == "")return;
+            this.things.push({
+                title: this.newthings,
+                isChecked: false,
+            });
+            this.newthings = "";
+            this.saveList();
+        },
+        deleteTodo: function(){
+          if(this.things.length <= 0){
+            return
+          }else{
+            result = confirm("本当に終わりましたか？");
+            if(result) {
+               this.things = this.things.filter(function(thing){
+                   return thing.isChecked === false;
+               }),
+               alert("お疲れ様でした!")
+            }else{
+ 
+            };
+            this.saveList();
+          }
+        },
+        saveList: function(){
+         localStorage.setItem('things', JSON.stringify(this.things));
+        },
+        loadList: function(){
+            this.things=JSON.parse(localStorage.getItem('things'));
+            if(!this.things){
+                this.things=[];
+            }
+        },
+        mounted: function(){
+            this.loadList();
+        }
     },
-
-    // ★STEP10 状態変更の処理
-    doChangeState: function (item) {
-      item.state = !item.state ? 1 : 0
-    },
-
-    // ★STEP10 削除の処理
-    doRemove: function (item) {
-      var index = this.todos.indexOf(item)
-      this.todos.splice(index, 1)
+    computed: {
+         setDate: function() {
+            hiduke = new Date();
+            year = hiduke.getFullYear();
+            month = hiduke.getMonth()+1;
+            day = hiduke.getDate();
+            return this.transfer_data = year + '/' + month + '/' + day ;
+         },
+         remaining: function() {
+             return this.things.filter(function(thing) {
+                 return thing.isChecked == true;
+             }).length;
+         },
     }
-
-  }
-})
+ })
